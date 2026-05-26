f7951a962805819ae97b8a790d31b09790cf97b9
Sg <suica@users.noreply.github.com>
2026-05-26 16:50:27 +0800

    Prove q > 0 in Rat.not_exist_sqrt_two minimal counterexample step.

--- Analysis/Section_4_4.lean
+++ Analysis/Section_4_4.lean
@@ -194,7 +194,16 @@ theorem Rat.not_exist_sqrt_two : ¬ ∃ x:ℚ, x^2 = 2 := by
       choose q hpos hq using hPp.2
       have : q^2 = 2 * k^2 := by linarith
       use q; constructor
-      . sorry
+      . by_contra h
+        simp_all
+        have h2: (2 * k)^2 ≤ q^2 := by
+          exact Nat.pow_le_pow_left h 2
+        rw [this] at h2
+        ring_nf at h2
+        have : k^2=0 := by grind
+        have : q^2 = 0 := by grind
+        observe : q = 0
+        linarith
       exact ⟨ hpos, k, by linarith [hPp.1], this ⟩
     have h1 : Odd (p^2) := by
       rw [odd_iff_exists_bit1]
